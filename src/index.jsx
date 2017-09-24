af5737fdbcb7fbe6af48105d8c977faf6f450331
shreyas-a <shreyas.a24@gmail.com>
2017-09-24 11:12:01 +0530

    added sass loader

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import Game from './Components/Game.jsx'
-import './style.css';
+import './style.scss';
 
 render(<Game/>, document.getElementById('root'));
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -20,8 +20,8 @@ module.exports = {
         use: "babel-loader"
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.(png|jpe?g|svg|gif|ico)/,
